621ac87faeffe4830a1885474ec0c8682819e044
Lizeth Guevara Capiral <zethguevara@gmail.com>
2022-11-23 13:29:15 +0100

    Home

--- frontendassessment/src/components/HomeComponent.js
+++ frontendassessment/src/components/HomeComponent.js
@@ -1,4 +1,9 @@
 import React from "react";
+import './HeaderComponent.css';
+import mapboxgl from 'mapbox-gl'; 
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnVuem95IiwiYSI6ImNsYXM5YWxmazAydTEzdGtqbm9ndjNiMWsifQ.esiqirmwHhQrHTxTIcYoSg';
 
 
 export function Home() {
@@ -8,8 +13,8 @@ export function Home() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis risus vitae erat ultricies hendrerit. Vivamus consequat velit lectus, a sagittis arcu luctus ac. Nam a ante commodo, vehicula leo vel, dignissim enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam porttitor ante ligula, a venenatis risus tristique sit amet. Donec nec blandit nibh, vel viverra ligula. In elementum posuere tortor quis placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse dapibus tellus id arcu auctor venenatis. Praesent porttitor pulvinar nibh feugiat imperdiet. Vivamus luctus maximus ex at vestibulum.
 Nullam mi quam, fringilla sit amet bibendum at, cursus nec velit. Vestibulum eget tincidunt sapien, a ultricies sem. Morbi ligula nulla, molestie at suscipit sed, aliquam at odio. Donec laoreet viverra risus, et convallis sem volutpat quis. Ut eget condimentum nisi, iaculis bibendum lectus. Nulla facilisi. In eu felis in odio consectetur cursus et vitae ipsum. Pellentesque nec ipsum quis lorem consectetur faucibus. Sed mollis, est in laoreet tincidunt, ante ante maximus leo, in condimentum leo libero ut enim. Sed hendrerit turpis sit amet augue eleifend, vel malesuada leo semper. Proin et ornare purus, vitae sodales odio. Proin id mauris sed tortor cursus condimentum vitae eu felis.</p>
         </div>
-
     )
+
 }
 
 export default Home;
--- frontendassessment/src/components/MainComponent.js
+++ frontendassessment/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
 import './HeaderComponent.css';
 import mapboxgl from 'mapbox-gl'; 
 import 'mapbox-gl/dist/mapbox-gl.css';
+
+
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnVuem95IiwiYSI6ImNsYXM5YWxmazAydTEzdGtqbm9ndjNiMWsifQ.esiqirmwHhQrHTxTIcYoSg';
 
@@ -11,6 +12,7 @@ export  class Main extends React.PureComponent {
 
     constructor(props) {
         super(props);
+
         this.state = {
             lng: 10.4437392,
             lat: 59.4101728,
@@ -18,25 +20,27 @@ export  class Main extends React.PureComponent {
         };
 
         this.mapContainer = React.createRef();
-        }
+    }
 
-        componentDidMount() {
-            const { lng, lat, zoom } = this.state;
-            const map = new mapboxgl.Map({
-                container: this.mapContainer.current,
-                style: 'mapbox://styles/mapbox/streets-v12',
-                center: [lng, lat],
-                zoom: zoom
-            });
-        }
+    componentDidMount() {
+        const { lng, lat, zoom } = this.state;
+        const map = new mapboxgl.Map({
+            container: this.mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    }
 
-        render() {
-            return (
-                <div>
-                <div ref={this.mapContainer} className="map-container" />
-                </div>
-            );
-        }
+    render() {
+        return (
+            <div>
+            <div ref={this.mapContainer} className="map-container" />
+            </div>
+        );
+    }
+
+
 }
     
 
